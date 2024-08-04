07f9094b1916f4d2e8a0ac1c607227616aff8315
Shahwaiz Islam <s1404islam@gmail.com>
2024-08-05 00:27:23 +0530

    VideoTitle-1

--- src/components/Browse/Browse.jsx
+++ src/components/Browse/Browse.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect } from "react";
 import Header from "../Header/Header";
 import useNowPlayingMovies from "../../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
   useNowPlayingMovies();
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <MainContainer />
+      <SecondaryContainer />
+    </div>
+  );
 };
 
 export default Browse;
