668cf7766b9f6a05bd44d17123f3d981f934c85d
Shahwaiz Islam <s1404islam@gmail.com>
2024-08-04 23:58:01 +0530

    Movie slice and dispatched data to it

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
+import moviesReducer from "./movieSlice";
 
 export const appStore = configureStore({
   reducer: {
     user: authReducer,
+    movies: moviesReducer,
   },
 });
--- src/components/Browse/Browse.jsx
+++ src/components/Browse/Browse.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect } from "react";
 import Header from "../Header/Header";
 import { API_OPTIONS, API_URL } from "../../utils/constant";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../../app/movieSlice";
 
 const Browse = () => {
+  const dispatch = useDispatch();
   const getNowPlayingMovies = async () => {
     const data = await fetch(API_URL, API_OPTIONS);
     const json = await data.json();
-    console.log(json);
+    //console.log(json.results);
+    dispatch(addNowPlayingMovies(json.results));
   };
 
   useEffect(() => {
