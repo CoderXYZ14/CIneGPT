876fc4b48935aa31fc5a4c1f2f03980d31703684
Shahwaiz Islam <s1404islam@gmail.com>
2024-08-04 18:07:58 +0530

    TMDB Api call

--- src/components/Browse/Browse.jsx
+++ src/components/Browse/Browse.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../Header/Header";
+import { API_OPTIONS, API_URL } from "../../utils/constant";
 
 const Browse = () => {
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(API_URL, API_OPTIONS);
+    const json = await data.json();
+    console.log(json);
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+    // Clean up the function
+  }, []);
   return <Header />;
 };
 
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,13 @@
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMDRmZWExOGZlZDJhN2QzOGE5OWI5MjBlMTU4MDNjYSIsIm5iZiI6MTcyMjc3NDQ5OC42MzMzNzcsInN1YiI6IjY2YWY3MjhmMjUyMWE2Zjc5NTViNzc4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.adID6Ca8riTNmI7j9Z2oZ43YX0AsoCR_HL5kfWY2vvs",
+  },
+};
+
+export const API_URL = "https://api.themoviedb.org/3/movie/now_playing?page=1";
